d181cd23b3ad1e9a25444f202810d14085972570
huangwx <hwx.simle@gmail.com>
2017-07-08 17:14:34 +0800

    saxpy may be ok

--- saxpy/saxpy.cu
+++ saxpy/saxpy.cu
@@ -38,6 +38,10 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     // these buffers are used in the call to saxpy_kernel below
     // without being initialized.
     //
+    int size = sizeof(float) * N;
+    cudaMalloc(&device_x, size);
+    cudaMalloc(&device_y, size);
+    cudaMalloc(&device_result, size);
 
     // start timing after allocation of device memory.
     double startTime = CycleTimer::currentSeconds();
@@ -45,10 +49,13 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     //
     // TODO: copy input arrays to the GPU using cudaMemcpy
     //
+    cudaMemcpy(device_x, xarray, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_y, yarray, size, cudaMemcpyHostToDevice);
 
     //
     // TODO: insert time here to begin timing only the kernel
     //
+    double cudaStartTime = CycleTimer::currentSeconds();
 
     // run saxpy_kernel on the GPU
     saxpy_kernel<<<blocks, threadsPerBlock>>>(N, alpha, device_x, device_y, device_result);
@@ -60,12 +67,14 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     // ensure the kernel running on the GPU has completed.  (Otherwise
     // you will incorrectly observe that almost no time elapses!)
     //
-    //cudaThreadSynchronize();
+    cudaThreadSynchronize();
 
+    double cudaEndTime = CycleTimer:currentSeconds();
 
     //
     // TODO: copy result from GPU using cudaMemcpy
     //
+    cudaMemcpy(resultarray, device_result, size, cudaMemcpyDeviceToHost);
 
     // end timing after result has been copied back into host memory.
     // The time elapsed between startTime and endTime is the total
@@ -84,6 +93,9 @@ saxpyCuda(int N, float alpha, float* xarray, float* yarray, float* resultarray) 
     //
     // TODO free memory buffers on the GPU
     //
+    cudaFree(device_x);
+    cudaFree(device_y);
+    cudaFree(device_result);
 }
 
 void
